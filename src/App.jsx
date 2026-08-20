a8c1f2560e1da8d8ff695a6e049d2206858649e2
EstebanGC <johan.gal.car@gmail.com>
2026-08-19 20:27:04 -0500

    Importing Authform

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import AuthForm from "./components/Authform";
 import Sidebar from "./components/Sidebar";
 import EvaluationForm from "./components/EvaluationForm";
 import SpeakingEvaluationForm from "./components/SpeakingEvaluationForm";
 import RubricTemplatesView from "./components/RubricTemplatesView";
+
 
 export default function App() {
   const [activeView, setActiveView] = useState("new");
